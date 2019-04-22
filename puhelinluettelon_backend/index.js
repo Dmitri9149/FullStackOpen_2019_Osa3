56233c556f5155d7a6a479df74b005b05523fc24
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-04-22 19:26:17 +0300

    persons data are added

--- puhelinluettelon_backend/index.js
+++ puhelinluettelon_backend/index.js
@@ -2,6 +2,26 @@ const express = require('express')
 const app = express()
 
 let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Martti Tienari",
+    "number": "040-123456",
+    "id": 2
+  },
+  {
+    "name": "Arto Järvinen",
+    "number": "040-123456",
+    "id": 3
+  },
+  {
+    "name": "Lea Kutvonen",
+    "number": "040-123456",
+    "id": 4
+  }
   
 ]
 
