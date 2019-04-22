f2d0c15f173c75ab315e09fd19b62ef6bdd219c0
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-04-22 19:24:07 +0300

    3.1.puhelinluettelon_backend

---
+++ puhelinluettelon_backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  
+]
+
+
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
